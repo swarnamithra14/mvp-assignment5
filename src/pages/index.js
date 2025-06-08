b179a51082bedbe1f254c84f0bd72474989e1ef0
swarnamithra14 <swarnamithra2006@gmail.com>
2025-06-09 00:38:03 +0530

    change 2

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
         <div className="space-x-4">
           <Link href="/contact">
             <a className="px-6 py-2 bg-green-600 text-white rounded hover:bg-green-700 transition">
-              Contact Us
+              Product
             </a>
           </Link>
           <Link href="/about">
